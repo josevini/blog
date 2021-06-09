4627b3eefaf83c5bdb0bbd8ae7f7ef7347295b65
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-06-09 17:04:09 -0300

    Create form-login.php

---
+++ includes/form-login.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="jumbotron">
+        <form action="login.php" method="post">
+            <div class="form-group">
+                <label class="sr-only">Usuário</label>
+                <input type="text" class="form-control" placeholder="Usuário" name="usuario" maxlength="15" required>
+            </div>
+            <div class="form-group">
+                <label class="sr-only">Senha</label>
+                <input type="password" class="form-control" placeholder="Senha" name="senha" maxlength="60" required>
+            </div>
+            <button type="submit" class="btn btn-success">Login</button>
+        </form>
+    </div>
+</div>
