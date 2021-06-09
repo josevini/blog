989c1cbe7d790fa88a7bcce0cb226aa3135c008d
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-06-09 17:04:29 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+    use App\Session\Login;
+    use App\Entity\Usuario;
+    require_once __DIR__."/vendor/autoload.php";
+    Login::requireLogout();
+    $erro_login = '';
+
+    $usuario = $_POST['usuario'] ?? null;
+    $senha = $_POST['senha'] ?? null;
+    if ($usuario && $senha) {
+        $novo_usuario = new Usuario();
+        $novo_usuario->usuario = $usuario;
+        $novo_usuario->senha = $senha;
+        $dados = $novo_usuario->buscaUsuario();
+
+        if ($dados) {
+            if (password_verify($novo_usuario->senha, $dados->senha)) {
+                Login::login($novo_usuario);
+            } else {
+                $erro_login = "Senha incorreta!";
+            }
+        } else {
+            $erro_login = "Usuário não encontrado!";
+        }
+    }
+    require_once "includes/header.php";
+    echo strlen($erro_login) ? '<div class="container"><div class="alert alert-danger">'.$erro_login.'</div></div>' : '';
+    require_once "includes/form-login.php";
+    require_once "includes/footer.php";
