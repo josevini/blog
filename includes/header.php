82cbf8bba270ef0f6519565bce8b32473f5c3640
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-06-09 17:04:18 -0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<?php
+    use App\Session\Login;
+    if (Login::isLogged()) {
+        $menu = file_get_contents("includes/menu-logado.php");
+    } else {
+        $menu = file_get_contents("includes/menu.php");
+    }
+?>
+
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <title></title>
+    </head>
+    <body class="bg-dark text-white">
+        <div class="jumbotron jumbotron-fluid p-0">
+            <div class="container-lg">
+                <nav class="navbar navbar-expand-lg navbar-light py-3">
+                    <a href="index.php" class="navbar-brand" style="font-weight: bold">LOGO</a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#op-menu">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="op-menu">
+                        <ul class="navbar-nav ml-auto mr-3">
+                            <li class="nav-item">
+                                <a href="index.php" class="nav-link">Home</a>
+                            </li>
+                            <?php echo $menu;?>
+                        </ul>
+                        <form method="get">
+                            <input type="search" class="form-control" placeholder="Pesquisar">
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        </div>
