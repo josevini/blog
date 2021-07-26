548ffca6e74a3fcbd4c6b337338f6d6abee51443
josevini <ferreiravini468@gmail.com>
2021-07-25 22:15:15 -0300

    Update edit-noticia.php

--- edit-noticia.php
+++ edit-noticia.php
@@ -1,1 +1,5 @@
 <?php
+    require_once __DIR__ . "/vendor/autoload.php";
+    require_once "includes/header.php";
+    require_once "includes/form-edit-noticia.php";
+    require_once "includes/footer.php";
