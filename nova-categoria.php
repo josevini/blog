d859b9b47893ba286083a146173b88b2fea1bbed
josevini <ferreiravini468@gmail.com>
2021-07-26 18:16:57 -0300

    Update nova-categoria.php

--- nova-categoria.php
+++ nova-categoria.php
@@ -1,17 +1,17 @@
 <?php
     use App\Session\Login;
-    use App\Entity\Categoria;
+    use App\Entity\Category;
     require_once __DIR__ . "/vendor/autoload.php";
     Login::requireLogin();
     $msg_final = '';
     $categoria = $_POST['categoria'] ?? null;
     if ($categoria) {
-        $nova_categoria = new Categoria();
-        $nova_categoria->categoria = $categoria;
-        if (!$nova_categoria->buscaCategoria()) {
-            $nova_categoria->cadastrar();
+        if (!Category::getCategoryByName($categoria)) {
+            $obCategory = new Category();
+            $obCategory->category = $categoria;
+            $obCategory->register();
         } else {
-            $msg_final = 'Esta categoria já existe!';
+            $msg_final = 'Esta categoria já foi cadastrada!';
         }
     }
     require_once "includes/header.php";
