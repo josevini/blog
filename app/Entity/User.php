9771771db6fa7a74fdad82da949c44d5c61c5839
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-26 18:33:38 -0300

    Create User.php

---
+++ app/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+    namespace App\Entity;
+    use App\DB\DB;
+    use \PDO;
+
+    class User {
+        public int $id;
+        public string $usuario;
+        public string $senha;
+        public string $nome;
+
+        public function cadastrar() {
+            $usuarios = new DB('usuarios');
+            $usuarios->insert([
+                'nome' => addslashes($this->nome),
+                'usuario' => addslashes($this->usuario),
+                'senha' => password_hash(addslashes($this->senha), PASSWORD_DEFAULT)
+            ]);
+            header('Location: login.php');
+            exit;
+        }
+
+        public function buscaUsuario() {
+            $usuarios = new DB('usuarios');
+            $res = $usuarios->select(where: "usuario = '{$this->usuario}'");
+            return $res->fetch(PDO::FETCH_OBJ);
+        }
+    }
