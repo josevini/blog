54c09b7759ff6ac8209cb82af43a33b4540675cc
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-27 18:18:15 -0300

    Update listagem.php

--- includes/listagem.php
+++ includes/listagem.php
@@ -3,16 +3,16 @@
         <?php
             use App\Entity\Post;
             use App\Session\Login;
-            $noticias = Post::getPosts();
-            foreach ($noticias as $noticia) {
+            $posts = Post::getPosts(order: 'id desc');
+            foreach ($posts as $post) {
                 echo "<div class='col-lg-4 col-md-6 col-sm-12'>";
                 echo "<div class='card text-dark my-2' style='height: 300px'>";
                 echo "<div class='card-body'>";
-                echo "<h4 class='card-title'>{$noticia->titulo}</h4>";
-                echo "<p class='card-text'>$noticia->texto</p>";
+                echo "<h4 class='card-title'>{$post->title}</h4>";
+                echo "<p class='card-text'>$post->content</p>";
                 if (Login::isLogged()) {
-                    echo "<a class='btn btn-success mr-2' href='edit-noticia.php?id=$noticia->id'>Editar</a>";
-                    echo "<a class='btn btn-danger' href='#'>Apagar</a>";
+                    echo "<a class='btn btn-success mr-2' href='edit-post.php?id=$post->id'>Editar</a>";
+                    echo "<a class='btn btn-danger' href='delete-post.php?id=$post->id'>Apagar</a>";
                 }
                 echo "</div>";
                 echo "</div>";
