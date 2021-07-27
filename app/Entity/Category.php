97b447ec80a26fae762c92c3adbb867babfdd9da
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-27 18:17:11 -0300

    Update Category.php

--- app/Entity/Category.php
+++ app/Entity/Category.php
@@ -8,20 +8,20 @@
         public string $category;
 
         public static function getCategoryById($id) {
-            return (new DB('categorias'))->select("id = '$id'")->fetch(PDO::FETCH_OBJ);
+            return (new DB('categories'))->select("id = '$id'")->fetch(PDO::FETCH_OBJ);
         }
 
         public static function getCategoryByName($name) {
-            return (new DB('categorias'))->select("categoria = '$name'")->fetch(PDO::FETCH_OBJ);
+            return (new DB('categories'))->select("category = '$name'")->fetch(PDO::FETCH_OBJ);
         }
 
         public static function getCategories($where = '', $order = '', $limit = '') {
-            return (new DB('categorias'))->select($where, $order, $limit)->fetchAll(PDO::FETCH_OBJ);
+            return (new DB('categories'))->select($where, $order, $limit)->fetchAll(PDO::FETCH_OBJ);
         }
 
         public function register() {
-            $this->id = (new DB('categorias'))->insert([
-                'categoria' => $this->category
+            $this->id = (new DB('categories'))->insert([
+                'category' => $this->category
             ]);
         }
     }
