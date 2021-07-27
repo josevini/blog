f68a453999706be015f15feeb3039c8bd26088f8
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-27 18:18:09 -0300

    Update form-login.php

--- includes/form-login.php
+++ includes/form-login.php
@@ -3,7 +3,7 @@
         <form action="login.php" method="post">
             <div class="form-group">
                 <label class="sr-only">Usuário</label>
-                <input type="text" class="form-control" placeholder="Usuário" name="usuario" maxlength="15" required>
+                <input type="text" class="form-control" placeholder="Usuário" name="usuario" maxlength="20" required>
             </div>
             <div class="form-group">
                 <label class="sr-only">Senha</label>
