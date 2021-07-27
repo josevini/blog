1fdc5d92d608a1978eb8982ae1abda46ab367025
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-27 18:17:40 -0300

    Delete menu-logado.php

--- includes/menu-logado.php
+++
@@ -1,9 +0,0 @@
-<li class="nav-item">
-    <a href="nova-noticia.php" class="nav-link">Notícia</a>
-</li>
-<li class="nav-item">
-    <a href="nova-categoria.php" class="nav-link">Categoria</a>
-</li>
-<li class="nav-item">
-    <a href="logout.php" class="nav-link">Sair</a>
-</li>
