6ac43b10e1016d7e5e3301537786c75b741db834
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-27 18:18:32 -0300

    Create form-new-category.php

---
+++ includes/form-new-category.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="jumbotron">
+        <form action="new-category.php" method="post">
+            <div class="form-group">
+                <label class="sr-only">Categoria</label>
+                <input type="text" class="form-control" placeholder="Categoria" name="category" maxlength="50" required>
+            </div>
+            <button type="submit" class="btn btn-success">Cadastrar</button>
+        </form>
+    </div>
+</div>
