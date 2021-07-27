784fe0bd8309964ad1639f3b36bd8be010be1616
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-27 18:18:34 -0300

    Create form-new-post.php

---
+++ includes/form-new-post.php
@@ -0,0 +1,28 @@
+<?php
+    use App\Entity\Category;
+?>
+<div class="container">
+    <div class="jumbotron">
+        <form action="new-post.php" method="post">
+            <div class="form-group">
+                <label class="sr-only">Título</label>
+                <input type="text" class="form-control" placeholder="Título" name="title" maxlength="100" required>
+            </div>
+            <div class="form-group">
+                <label class="sr-only">Categoria</label>
+                <select class="custom-select" name="category">
+                    <?php
+                        foreach (Category::getCategories() as $category) {
+                            echo "<option value='$category->id'>".ucfirst($category->category)."</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <label class="sr-only">Texto</label>
+                <textarea class="form-control" rows="5" name="content" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-success">Publicar</button>
+        </form>
+    </div>
+</div>
