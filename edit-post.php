c855e1ccf51cfbf480da62f667d6664070384cd9
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-29 03:40:12 -0300

    Update edit-post.php

--- edit-post.php
+++ edit-post.php
@@ -3,25 +3,48 @@
     use App\Session\Login;
     use App\Entity\Post;
     use App\Utils\Message;
-    $msg = '';
+    Login::requireLogin();
 
-    if (!Login::isLogged()) {
-        header('Location: login.php');
+    $msg = '';
+    $msg_title = '';
+
+    if (!isset($_GET['id'])) {
+        header('Location: index.php');
+        exit;
     }
 
-    $id = $_POST['id'] ?? null;
+    $id = $_GET['id'];
+    $post = Post::getPostById($_GET['id']);
+
+    if (!$post instanceof Post) {
+        header('Location: index.php');
+        exit;
+    }
+
     $title = $_POST['title'] ?? null;
     $content = $_POST['content'] ?? null;
 
-    if ($id && $title && $content) {
-        $obPost = new Post();
-        $obPost->title = $title;
-        $obPost->content = $content;
-        $obPost->editPostById($id);
-        header('Location: index.php');
+    if ($title && $content) {
+        if (Post::postExists($title)) {
+            if ($id == Post::getPostByTitle($title)->id) {
+                $post->title = $title;
+                $post->content = $content;
+                $post->editPostById($id);
+                $msg = Message::success('Notícia editada com sucesso!');
+                $msg_title = Message::alert('Título mantido!');
+            } else {
+                $msg = Message::error('Este título já está sendo usado!');
+            }
+        } else {
+            $post->title = $title;
+            $post->content = $content;
+            $post->editPostById($id);
+            $msg = Message::success('Notícia editada com sucesso!');
+        }
     }
 
     require_once "includes/header.php";
     echo strlen($msg) ? $msg : '';
+    echo strlen($msg_title) ? $msg_title : '';
     require_once "includes/form-edit-post.php";
     require_once "includes/footer.php";
