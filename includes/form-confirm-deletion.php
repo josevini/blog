f920e63281b0d0f166e6baf1c7fff6662bfe7668
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-29 15:21:11 -0300

    Create form-confirm-deletion.php

---
+++ includes/form-confirm-deletion.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="jumbotron">
+        <form method="post">
+            <div class="form-group">
+                <label class="sr-only">Usuário</label>
+                <input type="text" class="form-control" placeholder="Usuário" name="user" maxlength="20" required>
+            </div>
+            <div class="form-group">
+                <label class="sr-only">Senha</label>
+                <input type="password" class="form-control" placeholder="Senha" name="password" maxlength="60" required>
+            </div>
+            <button type="submit" class="btn btn-success">Confirmar</button>
+        </form>
+    </div>
+</div>
