4f7a8bc8528335bec28a1eb655d1cf1471ca7be0
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-07-28 22:55:15 -0300

    Update new-category.php

--- new-category.php
+++ new-category.php
@@ -2,6 +2,7 @@
     require_once __DIR__ . "/vendor/autoload.php";
     use App\Session\Login;
     use App\Entity\Category;
+    use App\Utils\Message;
     Login::requireLogin();
     $msg = '';
     $category = $_POST['category'] ?? null;
@@ -10,11 +11,12 @@
             $obCategory = new Category();
             $obCategory->category = $category;
             $obCategory->register();
+            $msg = Message::success('Categoria cadastrada com sucesso!');
         } else {
-            $msg_final = 'Esta categoria já foi cadastrada!';
+            $msg = Message::alert('Esta categoria já foi cadastrada!');
         }
     }
     require_once "includes/header.php";
-    echo strlen($msg) ? '<div class="container"><div class="alert alert-danger">'.$msg.'</div></div>' : '';
+    echo strlen($msg) ? $msg : '';
     require_once "includes/form-new-category.php";
     require_once "includes/footer.php";
