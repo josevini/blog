30e7428b012ef70537519c264a9881140d0769b3
Vinicius <ferreiravini468@gmail.com>
2022-04-21 23:36:53 -0300

    Rename login form atributtes

--- includes/form-login.php
+++ includes/form-login.php
@@ -3,11 +3,11 @@
         <form method="post">
             <div class="form-group">
                 <label class="sr-only">Usuário</label>
-                <input type="text" class="form-control" placeholder="Usuário" name="usuario" maxlength="20" required>
+                <input type="text" class="form-control" placeholder="Usuário" name="user" maxlength="20" required>
             </div>
             <div class="form-group">
                 <label class="sr-only">Senha</label>
-                <input type="password" class="form-control" placeholder="Senha" name="senha" maxlength="60" required>
+                <input type="password" class="form-control" placeholder="Senha" name="password" maxlength="60" required>
             </div>
             <button type="submit" class="btn btn-success">Login</button>
         </form>
