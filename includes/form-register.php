c6b2c894a633b998e5ca53fe3ca7a8a5027b9df2
Vinicius <ferreiravini468@gmail.com>
2022-04-21 23:40:16 -0300

    Rename register form atributtes

--- includes/form-register.php
+++ includes/form-register.php
@@ -3,15 +3,15 @@
         <form method="post">
             <div class="form-group">
                 <label class="sr-only">Nome completo</label>
-                <input type="text" class="form-control" placeholder="Nome completo" name="nome" maxlength="100" required>
+                <input type="text" class="form-control" placeholder="Nome completo" name="name" maxlength="100" required>
             </div>
             <div class="form-group">
                 <label class="sr-only">Usuário</label>
-                <input type="text" class="form-control" placeholder="Usuário" name="usuario" maxlength="20" required>
+                <input type="text" class="form-control" placeholder="Usuário" name="user" maxlength="20" required>
             </div>
             <div class="form-group">
                 <label class="sr-only">Senha</label>
-                <input type="password" class="form-control" placeholder="Senha" name="senha" maxlength="60" required>
+                <input type="password" class="form-control" placeholder="Senha" name="password" maxlength="60" required>
             </div>
             <button type="submit" class="btn btn-success">Cadastrar-se</button>
         </form>
